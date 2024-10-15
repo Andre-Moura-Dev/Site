0385fce954db859142e7b37edd1787b07a07b420
Andre-Moura-Dev <andreoliveiramoura06@gmail.com>
2024-10-15 13:49:53 -0300

    Commit site computadores

---
+++ JS/login.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('login-form').addEventListener('submit', function(event) {
+
+        //Impedir o envio do formulário, caso algum dado esteja errado.
+        event.preventDefault();
+
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+
+        //Tratamento de Erros do login, caso algo esteja errado
+        if(username == 'admin' && password == '896490') {
+            document.getElementById('login-message').textContent = 'Acesso Garantido';
+            document.getElementById('login-message').style.color = 'green';
+            alert("Bem vindo Administrador do site.");
+            location.href = ('index.html');
+        } else {
+            document.getElementById('login-message').textContent = 'Acesso Negado';
+            document.getElementById('login-message').style.color = 'red';
+            alert("Voçe não é o Administrador do site.");
+            location.href = ('login.html');
+        }
+    });
+});
